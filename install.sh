48982777d76471cab9c5afa2657dc2fc077e766c
Danny Haslund <danny@deltaplan.dk>
2025-10-28 14:38:46 +0100

    wip

--- install.sh
+++ install.sh
@@ -62,6 +62,32 @@ echo "MAKEOPTS=\"-j$(( $(nproc) + 1 ))\"" >> /mnt/gentoo/etc/portage/make.conf
 
 # TODO: ADD useflags here once I know the onces to use
 
+LRED='\033[01;31m'
+GREEN='\033[0;32m'
+LCYAN='\033[1;36m'
+LBLUE='\033[1;34m'
+LPURPLE='\033[0;35m'
+DGRAY='\033[1;30m'
+NC='\033[0m' # No Color
+
+
+
+message ()
+{
+  echo
+  echo -e " $LBLUE>>> $LRED $* $NC"
+}
+
+command ()
+{
+  echo -e "$LCYAN$*$NC"
+  "$@"
+  if [ $? -ne 0 ]; then
+    echo -e "$LREDFailed$NC"
+    return 1
+  fi
+}
+
 
 # Copy DNS info
 cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
@@ -82,5 +108,21 @@ export PS1="(chroot) ${PS1}"
 
 echo "GENTOO_MIRRORS=\"https://mirrors.dotsrc.org/gentoo http://mirrors.dotsrc.org/gentoo\"" >> /mnt/gentoo/etc/portage/make.conf
 
-emerge-webrsync -q
-emerge --sync --quiet
+message "Installing portage snapshot"
+command emerge-webrsync
+message "Updating portage tree"
+command emerge --sync
+
+message "Installing cpuid2cpuflags"
+command emerge --q --oneshot app-portage/cpuid2cpuflags
+
+message "installing gcc!"
+command emerge --q --oneshot sys-devel/gcc
+echo "install is done!"
+
+CPU_ARCH="$(gcc -march=native -Q --help=target | grep -- '-march=' | cut -f3 | cut -d ' ' -f 1 -z)"
+CFLAGS="-march=${CPU_ARCH} -mtune-${CPU_ARCH} -O3 -pipe -fno-plt -pthread -fsanitize=bounds,alignment,object-size -fsanitize-undefined-trap-on-error \
+        -fvisibility=hidden -fexceptions -Wformat -Werror=format-security \
+        -Wvla -Wimplicit-fallthrough -Wno-unused-result -Wno-unneeded-internal-declaration -Warray-bounds"
+CPU_FLAGS=$(cpuid2cpuflags | cut -c 1-15 --complement)
+
