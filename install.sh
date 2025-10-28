068abe316dd2e52dc87afa2b4e7bbf683caf38d5
Danny Haslund <danny@deltaplan.dk>
2025-10-28 12:52:59 +0100

    test

--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@ i=0; arr1=()
 while IFS='' read -r value; do
     arr1+=("$value")
 done <<-EOT
-$(printf '%s\n' fdisk -l |grep -w '^Disk /dev' |cut -d ' ' -f 1 --complement |cut -d ',' -f 1)
+$(printf '%s\n' fdisk -l | grep -w '^Disk /dev' | cut -d ' ' -f 1 --complement | cut -d ',' -f 1)
 EOT
 
 printf '%s\n' "${arr1[@]}"
 
-DISK_OPTIONS=$(fdisk -l |grep -w '^Disk /dev' |cut -d ' ' -f 1 --complement |cut -d ',' -f 1)
+DISK_OPTIONS=$(fdisk -l | grep -w '^Disk /dev' | cut -d ' ' -f 1 --complement | cut -d ',' -f 1)
 
-
+printf '%s\n' "${DISK_OPTIONS}"
 
 
 DISK=$(read -r -p "What disk do you want to use? - (/dev/sda): ")
